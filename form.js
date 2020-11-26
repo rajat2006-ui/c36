838797a58560a8618ad188721f29db9239aca43e
rajat2006-ui <73226257+rajat2006-ui@users.noreply.github.com>
2020-11-26 12:06:45 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,32 @@
+class Form{
+  constructor(){
+
+  }  
+
+  display(){
+      var title=createElement('h1','CAR RACING GAME')
+      title.position(130,0)
+
+      var button=createButton('play')
+      button.position(250,200)
+
+      var input=createInput("name")
+      input.position(130,160)
+
+      var greeting=createElement('h3')
+      
+      button.mousePressed(
+          function(){
+              input.hide()
+              button.hide()
+              var name=input.value();
+              playerCount=playerCount+1;
+              player.update(name)
+              player.update(playerCount)
+              
+              greeting.html('hello  '+name)
+              greeting.position(130,160);
+          }
+      )
+  }
+}
